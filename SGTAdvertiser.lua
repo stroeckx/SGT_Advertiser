348c82dc8de74fa63972e0817e2b3e83a9885eaf
Stijn Stroeckx <stijnstroeckx@hotmail.com>
2023-08-28 11:52:23 +0100

    Updated for patch 1.1.5
    - Updated for patch 1.1.5
    - Added icon in addon list

--- SGTAdvertiser.lua
+++ SGTAdvertiser.lua
@@ -4,7 +4,7 @@ SGTAdvertiser.L = LibStub("AceLocale-3.0"):GetLocale("SGTAdvertiser");
 --Variables start
 SGTAdvertiser.majorVersion = 1;
 SGTAdvertiser.subVersion = 0;
-SGTAdvertiser.minorVersion = 3;
+SGTAdvertiser.minorVersion = 4;
 SGTAdvertiser.macroButton = nil;
 SGTAdvertiser.searchMacroButton = nil;
 local priceFrame = nil;
